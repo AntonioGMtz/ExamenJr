70360a44e39141b7701fc0450eaf43e2fad500a9
Antonio <antoniogallegos086@gmail.com>
2022-02-24 01:56:47 -0600

    UI

--- app/src/main/java/com/example/examenjr/presentation/MovieViewModel.kt
+++ app/src/main/java/com/example/examenjr/presentation/MovieViewModel.kt
@@ -1,4 +1,26 @@
 package com.example.examenjr.presentation
 
-class MovieViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
+import com.example.examenjr.core.Resource
+import com.example.examenjr.repository.MovieRepository
+import kotlinx.coroutines.Dispatchers
+
+class MovieViewModel(private val repo: MovieRepository) : ViewModel() {
+
+    fun fetchMainScreenMovies() = liveData(Dispatchers.IO) {
+        emit(Resource.Loading())
+        try {
+            emit(Resource.Success(Triple(repo.getUpcomingMovies(),repo.getTopRatedMovies(),repo.getPopularMovies())))
+        } catch (e: Exception) {
+            emit(Resource.Failure(e))
+        }
+    }
+}
+
+class MovieViewModelFactory(private val repo: MovieRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(MovieRepository::class.java).newInstance(repo)
+    }
 }
